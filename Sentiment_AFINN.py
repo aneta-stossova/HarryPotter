3af4ee082eb52c3819d11dde198f76070d2312c8
aneta-stossova <167459083+aneta-stossova@users.noreply.github.com>
2025-04-15 20:16:46 +0200

    Sentiment Analysis (Python)

---
+++ Sentiment_AFINN.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from afinn import Afinn
+
+dialogues = pd.read_csv("CSV_raw_data/Tables/Dialogue.csv", encoding="ISO-8859-1")
+fact_dialogue = pd.read_csv("CSV_draft/fact_dialogue_afinn_draft.csv", encoding="ISO-8859-1")
+
+# Afinn sentiment analyzer
+afinn = Afinn()
+
+dialogues["SENTIMENT_AFINN"] = dialogues["Dialogue"].apply(afinn.score)
+
+sentiment_map = dict(zip(dialogues["Dialogue ID"], dialogues["SENTIMENT_AFINN"]))
+
+fact_dialogue["SENTIMENT_AFINN"] = fact_dialogue["ID_DIALOGUE"].map(sentiment_map)
+
+fact_dialogue.to_csv("CSV_final/fact_table_afinn_final.csv", index=False)
+print("The table was successfully created")
