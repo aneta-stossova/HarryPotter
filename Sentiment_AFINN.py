30f46e65465c7472902d2a9e502ecd0f6ea8cd36
aneta-stossova <167459083+aneta-stossova@users.noreply.github.com>
2025-04-15 20:09:18 +0200

    Sentiment Analysis (Python)

--- Sentiment_AFINN.py
+++ Sentiment_AFINN.py
@@ -1,16 +1,17 @@
 import pandas as pd
 from afinn import Afinn
 
-dialogues = pd.read_csv("/data/in/tables/DIALOGUE.csv")
-fact_dialogue = pd.read_csv("/data/in/tables/FACT_DIALOGUE.csv")
+dialogues = pd.read_csv("CSV_raw_data/Tables/Dialogue.csv", encoding="latin1")
+fact_dialogue = pd.read_csv("CSV_draft/fact_dialogue_afinn_draft.csv", encoding="latin1")
 
 # Afinn sentiment analyzer
 afinn = Afinn()
 
 dialogues["SENTIMENT_AFINN"] = dialogues["Dialogue"].apply(afinn.score)
 
-sentiment_map = dict(zip(dialogues["Dialogue_ID"], dialogues["SENTIMENT_AFINN"]))
+sentiment_map = dict(zip(dialogues["Dialogue ID"], dialogues["SENTIMENT_AFINN"]))
 
 fact_dialogue["SENTIMENT_AFINN"] = fact_dialogue["ID_DIALOGUE"].map(sentiment_map)
 
-fact_dialogue.to_csv("/data/out/tables/FACT_TABLE_AFINN_DENI.csv", index=False)
+fact_dialogue.to_csv("CSV_final/fact_table_afinn_final.csv", index=False)
+print("The table was successfully created")
